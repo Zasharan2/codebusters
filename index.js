590c3bae8b909544745385ec6a7ede6a8d08771a
Zasharan2 <65794527+Zasharan2@users.noreply.github.com>
2023-01-25 10:50:40 -0500

    fixed X->X sub problem

--- index.js
+++ index.js
@@ -192,7 +192,18 @@ function getQuote() {
 
         // encrypt quote
         console.log(quote);
-        encryptArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25].sort((a, b) => 0.5 - Math.random());
+        var goodArray = false;
+        var goodArrayCheck = false;
+        while (!goodArray) {
+          encryptArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25].sort((a, b) => 0.5 - Math.random());
+          goodArrayCheck = true;
+          for (var m = 0; m < encryptArray.length; m++) {
+            if (encryptArray[m] == m) {
+              goodArrayCheck = false;
+            }
+          }
+          goodArray = goodArrayCheck;
+        }
         encryptedQuote = "";
         for (var i = 0; i < quote.length; i++) {
             if (!((quote[i].match(symbolRegex) || []).length > 0)) {
